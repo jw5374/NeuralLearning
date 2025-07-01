added He Uniform heuristic for weight initialization, normalized input values to be between 0-1, added some type hinting, fixed softmax overflows

lib.py
@@ -1,31 +1,45 @@
+from numpy.typing import NDArray
 import numpy as np
 
 
 # ********** ACTIVATION FUNCTIONS **********
-def reLU(input):
+def reLU(input: NDArray[float | int]) -> NDArray[float]:
     # derivative: 0 if x < 0, 1 if x > 0
     return np.maximum(0, input)
 
 
-def leaky_ReLU(input):
+def leaky_ReLU(input: NDArray[float | int]) -> NDArray[float]:
     # derivative: 0.01 if x < 0, 1 if x > 0
     return np.maximum((0.01 * input), input)
 
 
-def sigmoid(input):
+def sigmoid(input: NDArray[float | int]) -> NDArray[float]:
     # derivative: sigmoid(input) * (1 - sigmoid(input))
     return 1 / (1 + np.exp(input * -1))
 
 
-def softmax(input):
-    return np.exp(input) / float(np.sum(np.exp(input)))
+def softmax(input: NDArray[float | int]) -> NDArray[float]:
+    # prevent overflow of input in exponentiation
+    subtract_max = input - np.max(input)
+    return np.exp(subtract_max) / np.sum(np.exp(subtract_max))
 
 
-def tanh(input):
+def tanh(input: NDArray[float | int]) -> NDArray[float]:
     # derivative: 1 - tanh(input)^2
     return (np.exp(input) - np.exp(input * - 1)) / (np.exp(input) + np.exp(input * - 1))
 
 
 # ********** LOSS FUNCTIONS **********
-def mse(input, expected):
-    return
+def mse(input: NDArray[float | int], expected: NDArray[float | int]) -> float:
+    sum = 0
+    for pred, targ in zip(input, expected):
+        sum += np.square(targ - pred)
+    return sum / input.size
+
+
+def cross_entropy(input: NDArray[float], expected: NDArray[float]) -> float:
+    # derivative with respect to any output neuron: (output of neuron at index i) - (target of neuron at index i)
+    sum = 0
+    for pred, targ in zip(input, expected):
+        sum += np.multiply(targ, np.log(pred))
+    return sum / -input.size

mnist.py
@@ -1,14 +1,30 @@
 # from keras.datasets import mnist
+from pprint import pprint
+from types import FunctionType
+from numpy.typing import NDArray
 import keras.src.datasets.mnist as mnist
 import numpy as np
 
 import lib
 
+np.set_printoptions(suppress=True)
+
 (images, labels), (testing_images, testing_labels) = mnist.load_data()
+
+
+# normalize input values?
+images = images.astype(float)
+for i, img in enumerate(images):
+    images[i] = img / 255
+testing_images = images.astype(float)
+for i, img in enumerate(testing_images):
+    testing_images[i] = img / 255
+
 
 mnist_image = images[0]
 for row in mnist_image:
     print("".join([" " if p == 0 else "@" for p in row]))
+
 
 print(np.shape(images))
 print(np.shape(labels))
@@ -24,48 +40,50 @@ BATCH_SIZE = 10
 
 
 class NNLayer:
-    weights = None
-    biases = None
-    nodes = None
-    node_count = 0
-    previous_layer_count = 0
+    weights: NDArray[NDArray[float]] = None
+    biases: NDArray[float] = None
+    nodes: NDArray[float | int] = None
+    node_count: int = 0
+    previous_layer_count: int = 0
 
-    def __init__(self, previous_layer_count, nodes=None, node_count=10):
+    def __init__(self, previous_layer_count: int, nodes: NDArray[float | int] = None, node_count: int = 10):
         if nodes is not None:
             self.nodes = nodes.flatten()
             self.node_count = self.nodes.size
             return
         self.node_count = node_count
         self.previous_layer_count = previous_layer_count
-        self.weights = np.random.uniform(-1.0, 1.0, self.previous_layer_count)
-        self.biases = np.random.uniform(0, 0.001, self.node_count)
-        self.nodes = np.zeros(self.node_count)
 
-    def feed_forward(self, input_nodes, activation_function):
+        # He Uniform (for ReLU and Leaky ReLU)
+        he_limit = np.sqrt(6 / previous_layer_count)
+        self.weights = (np.random.uniform(-he_limit, he_limit, (self.previous_layer_count, self.node_count)))
+
+        # constant biases?
+        self.biases = np.full(self.node_count, 0.00001)
+        print(np.shape(self.weights))
+
+    def feed_forward(self, input_nodes: NDArray[float | int], activation_function: FunctionType) -> NDArray[float | int]:
         if input_nodes.size != self.previous_layer_count:
             raise Exception("Number of Weights and Biases should match previous input size")
-        i = 0
-        linear_combination = 0
-        while i < self.node_count:
-            linear_combination = np.vecdot(input_nodes, self.weights) + self.biases[i]
-            self.nodes[i] = linear_combination
-            i += 1
+
+        # multiplies input vector against weight matrix and adds biases element wise
+        self.nodes = np.add(np.vecmat(input_nodes, self.weights), self.biases)
         self.nodes = activation_function(self.nodes)
-        print("activation?", self.nodes)
         return self.nodes
 
-    def back_prop(self):
-        pass
+    def back(self, previous_layer_nodes: NDArray[float], gradient_function: FunctionType) -> NDArray[float]:
+
+        return self.nodes
 
 
 class NeuralNet:
-    input_set = []
-    input_layer = None
-    hidden_layers = []
-    output = None
-    forward_outputs = []
+    input_set: list[NDArray] = []
+    input_layer: NDArray = None
+    hidden_layers: list[NNLayer] = []
+    output: NNLayer = None
+    forward_outputs: list[NDArray] = []
 
-    def __init__(self, input_set, hidden_layer_count=2, hidden_layer_size=16, output_size=9):
+    def __init__(self, input_set: list[NDArray], hidden_layer_count: int = 2, hidden_layer_size: int = 16, output_size: int = 10):
         self.input_set = input_set
         prev_layer_size = self.input_set[0].flatten().size
         for i in range(hidden_layer_count):
@@ -74,10 +92,10 @@ class NeuralNet:
             prev_layer_size = hl.node_count
         self.output = NNLayer(prev_layer_size, node_count=output_size)
 
-    def forward(self):
+    def forward(self) -> list[NDArray]:
         # we need read every image of our input set
         # TODO: potentially do a matrix calculation for input
-        for inp in self.input_set[:5]:
+        for inp in self.input_set:
             # create layer for current image we are reading
             self.input_layer = NNLayer(0, inp)
 
@@ -90,20 +108,34 @@ class NeuralNet:
                 curr_layer += 1
                 curr_layer_output = self.hidden_layers[curr_layer].feed_forward(previous_layer_output, lib.leaky_ReLU)
                 previous_layer_output = curr_layer_output
-            print(previous_layer_output)
             final_output = self.output.feed_forward(previous_layer_output, lib.softmax)
             self.forward_outputs.append(final_output)
+        return self.forward_outputs
+
+    def back_prop(self):
+        # need to calculate derivative of loss function for output layer
+
+        # then need to calculate partial derivatives in all hidden layers for each weight
+
+        # multiply each partial derivative with the multiplied values of previous derivatives each layer you go down
+
+        # subtract the value of the current weight that you're on by (LEARNING_RATE * value of the partial derivative)
+        for hl in self.hidden_layers:
+
+            continue
+
+        pass
 
 
 # input layer (784 nodes, one for each pixel)
 images_flattened = [image.flatten() for image in images]
 image_size = images_flattened[0].size
 
-nn = NeuralNet(images_flattened)
+nn = NeuralNet(images_flattened, hidden_layer_size=100)
 
 nn.forward()
 
-print(nn.forward_outputs)
+pprint(len(nn.forward_outputs))
 
 
 # input_l = NNLayer(0, nodes=images_flattened[0])